44a0d7e7cf3b3b6d0149420f5687805e8f8457fe
madalamanikanta <madalamanikanta7075@gmail.com>
2025-07-08 18:32:43 +0000

    data types

---
+++ 1_basic/1_variables.js
@@ -0,0 +1,11 @@
+const accound_Id = 5376033
+let acc_email="madalamani@gmail.com"
+var acc_pass="12345"
+acc_city="Hyd"
+// accound_Id=2 // Not allowed
+/* 
+ don't use va4 because of issue with block scope and functional scope
+*/
+acc_email="fre"
+acc_pass="234"
+console.table([accound_Id,acc_email,acc_pass])
---
+++ 1_basic/2_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version
+// alert( 3 + 3 ) // don't work (using node)
+
+let name="Manikanta" // string
+let age = 19//Number
+let is_loged_in=false //boolean
+let state;//null =>Standalone value
+// undefined
+//symbol =>unique
+// object
