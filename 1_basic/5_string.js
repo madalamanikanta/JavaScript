228e6c37077bacb79515b2424203a56a356d13a7
madalamanikanta <madalamanikanta7075@gmail.com>
2025-07-12 07:15:30 +0000

    string

---
+++ 1_basic/5_string.js
@@ -0,0 +1,10 @@
+let name ="meeee"
+// console.log(` Hello my name is ${name}`)
+const gamename= new String ('manik@#$53t4y5z6xio8kjanta')
+// console.log(gamename);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toWellFormed());
+console.log(gamename.indexOf('k'));
+console.log(gamename.substring(0 ,5))
+console.log(gamename.slice(-9 , -1));
--- 1_basic/datatype_summary.js
+++ 1_basic/datatype_summary.js
@@ -1,3 +1,11 @@
-// primative :7  String , Number , Boolearn , null , undefined , Symbol, BingInt
+// primative :7  String , Number , Boolearn , null , undefined , Symbol, BingInt  Memory(Stack)
 
-//Reference (non-primitive) : Array , object , Function
+//Reference (non-primitive) : Array , object , Function   memory:(Heap)
+let userone ={
+    email: " madalamanikanta@gmail.com"
+
+}
+let userTwo=userone
+userTwo.email="mmmmmmmmmmmm"
+console.log(userTwo);
+console.log(userone); // both chamges!
