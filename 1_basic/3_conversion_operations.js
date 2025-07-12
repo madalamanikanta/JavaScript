2e02b1bb74b3b67f420d4ac55826d8b9f205e2d2
madalamanikanta <madalamanikanta7075@gmail.com>
2025-07-12 06:14:27 +0000

    3,4

---
+++ 1_basic/3_conversion_operations.js
@@ -0,0 +1,7 @@
+let score = "38eg"
+let score_conversion = Number(score) //NaN when it has chars, undefined.. // null=>None3
+// console.log(score_conversion)
+// console.log(typeof score)
+let bool_value=1
+let value= Boolean(bool_value)
+console.log(value);
---
+++ 1_basic/datatype_summary.js
@@ -0,0 +1,3 @@
+// primative :7  String , Number , Boolearn , null , undefined , Symbol, BingInt
+
+//Reference (non-primitive) : Array , object , Function
