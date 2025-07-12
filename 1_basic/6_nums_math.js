9c0d9dbd8e29df5c625b415be3d18de83dc6e1a5
madalamanikanta <madalamanikanta7075@gmail.com>
2025-07-12 16:26:38 +0000

    nums and math

---
+++ 1_basic/6_nums_math.js
@@ -0,0 +1,15 @@
+const score = 400
+const numb=new Number(1000)
+console.log(numb);
+console.log(numb.toFixed(2));
+console.log(1000000.09652.toPrecision(8));
+
+
+// *******************Maths*****************************************
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.6));
+console.log(Math.ceil(50.252));
+console.log(Math.floor(52.52));
+console.log(Math.min(85,852,85,852));
+// max ,random
